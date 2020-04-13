80fde738526f2ac8ea2b1b25a98936bf6c1d6852
Cesar Guibo <cesarguibo0@gmail.com>
2020-04-13 13:13:25 -0300

    Melhora nos comentarios

--- Calculadora.asm
+++ Calculadora.asm
@@ -1,396 +1,298 @@
-	.data
+# Dvisao deve retornar o resto ?
+# TODO Strings que devem ser impressas
+# TODO imprime_resultado
+# TODO raiz_quadrada
+# TODO fatorial
+# TODO imc
+# TODO fibonacci
+# TODO tabuada 
+    .data
 
-	.align 0
-requerir_id_operacao: .asciiz "\n"
-output_resultado: 		.space 50
+    .align 0
+requerir_id_operacao:           .asciiz "\n"
+requerir_operando1_soma:        .asciiz "\n"
+requerir_operando2_soma:        .asciiz "\n"
+requerir_operando1_sub:         .asciiz "\n"
+requerir_operando2_sub:         .asciiz "\n"
+requerir_operando1_mult:        .asciiz "\n"
+requerir_operando2_mult:        .asciiz "\n"
+requerir_operando1_div:         .asciiz "\n"
+requerir_operando2_div:         .asciiz "\n"
+requerir_operando1_pot:         .asciiz "\n"
+requrir_operando2_pot:          .asciiz "\n"
+requerir_operando_raiz:         .asciiz "\n"
+requerir_operando_tabuada:      .asciiz "\n"
+requerir_operando1_imc:         .asciiz "\n"
+requerir_operando2_imc:         .asciiz "\n"
+requerir_operando_fatorial:     .asciiz "\n"
+requerir_operando_fibonacci:    .asciiz "\n"
+resultados:                     .asciiz "\n"
 
-	.align 2
-jump_table: .word case_sair, case_soma, case_subtracao, case_multiplicacao, 
-		          case_divisao, case_potencia, case_raiz_quadrada, case_tabuada,
-		  	      case_imc, case_fatorial, case_fibonacci, case_default
+    .align 2
+cases_table: .word case_sair, case_soma, case_subtracao, case_multiplicacao, 
+                  case_divisao, case_potencia, case_raiz_quadrada, case_tabuada,
+                  case_imc, case_fatorial, case_fibonacci
 
 
-	.text
-main:	
-	la $s0, jump_table
-	la $s1, requerir_id_operacao
+    .text
+main:   
+    la $s0, cases_table                     # Carrega o vetor de labels cases_table em $s0
+    la $s1, requerir_id_operacao            # Carrega a string que pede o id_operacao em $s1
+
 main_loop:
-	li $v0, 4 										# $v0 = 4, syscall imprime string
-	move $a0, $s1 									# $a0 = $s1, seleciona requerir_id_operacao
-	syscall 										# Imprime requerir_id_operacao
+    li $v0, 4                               # $v0 = 4, syscall imprime string
+    move $a0, $s1                           # $a0 = $s1, seleciona requerir_id_operacao
+    syscall                                 # Imprime requerir_id_operacao
 
-	li $v0, 5 										# $v0 = 5, syscall le int
-	syscall 										# Le o id da operacao
-sll $t0, $v0, 2 								# $t0 = id da operacao * 4
-	add $t0, $t0, $s0 								# $t0 = &jump_table[id_operacao]
-	lw $t1, 0($t0) 									# $t1 = jump_table[id_operacao] 
-	jr $t1 											# Jump to jump_table[id_operacao] 
+    li $v0, 5                               # $v0 = 5, syscall le int
+    syscall                                 # Le o id da operacao
+
+    # Verifica se o id_operacao e invalido 
+    slti $t0, $v0, 11                       # $t0 = ($v0 < 11)
+    beq $t0, $zero, id_operacao_invalido    # Se $t0 = false jump to id_operacao_invalido
+
+    # Vai para o case da operacao selecionada
+    sll $t0, $v0, 2                         # $t0 = id da operacao * 4
+    move $t0, $s0                           # $t0 = &cases_table[id_operacao]
+    lw $t1, 0($t0)                          # $t1 = cases_table[id_operacao] 
+    jr $t1                                  # Jump to cases_table[id_operacao] 
 
 case_sair:
-	j main_loop_end 								# Sai do loop
+    j main_loop_end                 # Sai do loop
 
 case_soma:
-#Colocar a string que pede os operandos
-	li $v0, 1
-	li $a0, 1
-	syscall
+    li $a0, 2                       # Carrega o numero de operandos em $a0
+    la $a1, requerir_operando1_soma # Carrega a string que pede o primeiro operando da soma em $a1 
+    la $a2, requerir_operando2_soma # Carrega a string que pede o segundo operando da soma em $a2
+    jal le_operandos                # Cham sub_rotina le operandos   
 
-	li $v0, 4
-	la $a0, requerir_id_operacao
-	syscall
+    move $a0, $v0                   # Carrega o primeiro operando obtido por le_operandos em $a0
+    move $a1, $v1                   # Carrega o segundo operando obtido por le_operandos em $a1
+    jal soma                        # Chama sub-rotina soma
 
-	li $v0, 5
-	syscall
-	move $a0, $v0
-
-	li $v0, 5
-	syscall
-	move $a1, $v0
-
-	jal soma
-	move $t0, $v0
-
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	li $v0, 1
-	move $a0, $t0
-	syscall
-
-	j main_loop
+    j cases_end                     
 
 case_subtracao:
-#Colocar a string que pede os operandos 
-	li $v0, 1
-	li $a0, 2
-	syscall
+    li $a0, 2
+    la $a1, requerir_operando1_sub
+    la $a2, requerir_operando2_sub
+    jal le_operandos    
 
-	li $v0, 4
-	la $a0, requerir_id_operacao
-	syscall
+    move $a0, $v0
+    move $a1, $v1
+    jal subtracao
 
-	li $v0, 5
-	syscall
-	move $a0, $v0
-
-	li $v0, 5
-	syscall
-	move $a1, $v0
-
-	jal subtracao
-	move $t0, $v0
-
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	li $v0, 1
-	move $a0, $t0
-	syscall
-
-	j main_loop 
+    j cases_end
 
 case_multiplicacao:
-#Colocar a string que pede os operandos
-	li $v0, 1
-	li $a0, 3
-	syscall
+    li $a0, 2
+    la $a1, requerir_operando1_mult
+    la $a2, requerir_operando2_mult
+    jal le_operandos    
 
-	li $v0, 4
-	la $a0, requerir_id_operacao
-	syscall
+    move $a0, $v0
+    move $a1, $v1
+    jal multiplicacao
 
-	li $v0, 5
-	syscall
-	move $a0, $v0
-
-	li $v0, 5
-	syscall
-	move $a1, $v0
-
-	jal multiplicacao
-	move $t0, $v0
-	
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	li $v0, 1
-	move $a0, $t0
-	syscall
-
-	j main_loop
+    j cases_end
 
 case_divisao:
-#Colocar a string que pede os operandos
-	li $v0, 1
-	li $a0, 4
-	syscall
+    li $a0, 2
+    la $a1, requerir_operando1_div
+    la $a2, requerir_operando2_div
+    jal le_operandos    
 
-	li $v0, 4
-	la $a0, requerir_id_operacao
-	syscall
+    move $a0, $v0
+    move $a1, $v1
+    jal divisao
 
-	li $v0, 5
-	syscall
-	move $a0, $v0
-
-	li $v0, 5
-	syscall
-	move $a1, $v0
-
-	jal divisao
-	move $t0, $v0
-
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	li $v0, 1
-	move $a0, $t0
-	syscall
-
-	j main_loop
+    j cases_end
 
 case_potencia:
-#Colocar a string que pede os operandos
-	li $v0, 1
-	li $a0, 5
-	syscall
-	
-	li $v0, 4
-	la $a0, requerir_id_operacao
-	syscall
+    li $a0, 2
+    la $a1, requerir_operando1_pot
+    la $a2, requerir_operando2_pot
+    jal le_operandos    
 
-	li $v0, 5
-	syscall
-	move $a0, $v0
+    move $a0, $v0
+    move $a1, $v1
+    jal potencia
 
-	li $v0, 5
-	syscall
-	move $a1, $v0
-
-	jal potencia
-	move $t0, $v0
-	
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	li $v0, 1
-	move $a0, $t0
-	syscall
-	
-	j main_loop
+    j cases_end
 
 case_raiz_quadrada:
-#Colocar a string que pede os operandos
-	li $v0, 5
-	syscall
-	move $v0, $a0
+    li $a0, 1
+    la $a1, requerir_operando_raiz
+    jal le_operandos    
 
-	jal raiz_quadrada
-	
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-	
-	move $a0, $v0
-	li $v0, 1
-	syscall
-
-	j main_loop
+    move $a0, $v0
+    jal raiz_quadrada
+    
+    j cases_end
 
 case_tabuada:
-#Colocar a string que pede os operandos
-	li $v0, 5
-	syscall
-	move $v0, $a0
+    li $a0, 1
+    la $a1, requerir_operando_tabuada
+    jal le_operandos    
 
-	jal raiz_quadrada
-	
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-	
-	move $a0, $v0
-	li $v0, 1
-	syscall
-
-	j main_loop
+    move $a0, $v0
+    jal raiz_quadrada
+    
+    j cases_end
 
 case_imc:
-#Colocar a string que pede os operandos
-	li $v0, 5
-	syscall
-	move $v0, $a0
+    li $a0, 2
+    la $a1, requerir_operando1_imc
+    la $a2, requerir_operando2_imc
+    jal le_operandos    
 
-	li $v0, 5
-	syscall
-	move $v0, $a1
-
-	jal imc
-	
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	move $a0, $v0
-	li $v0, 1
-	syscall
-
-	j main_loop
+    move $a0, $v0
+    move $a1, $v1
+    jal imc
+    
+    j cases_end
 
 case_fatorial:
-#Colocar a string que pede os operandos
-	li $v0, 5
-	syscall
-	move $v0, $a0
+    li $a0, 1
+    la $a1, requerir_operando_fatorial
+    jal le_operandos    
 
-	jal fatorial
+    move $a0, $v0
+    jal fatorial
 
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
-
-	move $a0, $v0
-	li $v0, 1
-	syscall
-
-	j main_loop
+    j cases_end
 
 case_fibonacci:
-#Colocar a string que pede os operandos
-	li $v0, 5
-	syscall
-	move $v0, $a0
+    li $a0, 1
+    la $a1, requerir_operando_fibonacci
+    jal le_operandos    
 
-	jal fatorial
+    move $a0, $v0
+    jal fibonacci
 
-	li $v0, 4
-	la $a0, output_resultado
-	syscall
+cases_end:
+    move $a0, $v0
+    jal imprime_resultado
+    
+    j main_loop
 
-	move $a0, $v0
-	li $v0, 1
-	syscall
+id_operacao_invalido:
+    la $a0, id_invalido
+    syscall
 
-	j main_loop
-
-case_default:
-	j main_loop
+    j main_loop
 
 main_loop_end:
-	li $v0, 10
-	syscall	
+    li $v0, 10
+    syscall 
 
 
+# Sub-rotina que receb uma parcela da soma em $a0,
+# outra em $a1 e retorna o resultado da soma em $v0
 soma:
-	slt $t0, $a0, $zero 					# $t0 = ($a0 < 0)
-	slt $t1, $a1, $zero 					# $t1 = ($a1 < 0)
-
-	and $t2, $t0, $t1 						# $t2 = (($a0 < 0) and ($a1 < 0))
-
-	# $t3 = (($a0 >= 0) and ($a1 >= 0))
-	or $t3, $t0, $t1 						# $t3 = (($a0 < 0) or ($a1 < 0))
-	xori $t3, $t3, 1 						# $t3 = (not (($a0 < 0) or ($a1 < 0)))		
+    add $v0, $a0, $a1   # $v0 = $a0 + $a1
+    jr $ra              
 
 
-	jr $ra 									# Volta para onde essa funcao foi chamada
-
-
+# Sub-rotina que receb o minuendo da subtracao em $a0,
+# o subtraendo em $a1 e retorna a diferenca em $v0
 subtracao:
-	# $a0 - $a1 = $a0 + (- $a1)
-	sub $a1, $zero, $a1 					# $$a1 = - $a1
-	jal soma
+    sub $v0, $a0, $a1   # $v0 = $a0 - $a1       
+    jr $ra               
 
-	sub $a1, $zero, $a1 					# Restaura $a1
-
-	jr $ra
-
+# Sub-rotina que receb um fator da multiplicacao em $a0,
+# outro em $a1 e retorna o produto em $v0
 multiplicacao:
-	mul $v0, $a0, $a1
-	jr $ra
+    mul $v0, $a0, $a1   # $v0 = $a0 * $a1
+    jr $ra
 
+# Sub-rotina que receb um dividendo em $a0,
+# um divisor em $a1 e retorna o quociente em $v0
 divisao:
-	div $v0, $a0, $a1	
-	jr $ra
+    div $v0, $a0, $a1   
+    jr $ra
 
+
+# Sub-rotina que recebe a base em $a0 e o expoente em $a1 e 
+# retorna o resultado de $ao^$a1.
 potencia:
-	beq $a1, 1, potencia_caso_base 				# Se o expoente for 1 jump to potencia_caso_base
-	
-	move $t0, $a0 								# Guarda o valor armazenado em $a0 em $t0
-	move $t1, $a1 								# Guarda o valor armazenado em $a1 em $t1
-	li $t2, 2 									# Deixa 2 carregado em $t2
+    beq $a1, 1, potencia_caso_base              # Se o expoente for 1 jump to potencia_caso_base
+    
+    move $t0, $a0                               # Guarda o valor armazenado em $a0 em $t0
+    move $t1, $a1                               # Guarda o valor armazenado em $a1 em $t1
+    li $t2, 2                                   # Deixa 2 carregado em $t2
 
-	subi $sp, $sp, 12 							# Cresce a stack em 12 posições 
-	sw $ra, 8($sp) 								# Salva $ra na stack
-	sw $a0, 4($sp) 								# Salva $a0 na stack
-	sw $a1, 0($sp) 								# Salva $a1 na stack
+    subi $sp, $sp, 12                           # Cresce a stack em 12 posições 
+    sw $ra, 8($sp)                              # Salva $ra na stack
+    sw $a0, 4($sp)                              # Salva $a0 na stack
+    sw $a1, 0($sp)                              # Salva $a1 na stack
 
-	mul $a0, $t0, $t0 							# $a0 = $t0 * $t0
-	div $a1, $t1, $t2 							# $a1 = $t2 / 2
-	jal potencia 								# Chama subrotina potencia
+    mul $a0, $t0, $t0                           # $a0 = $t0 * $t0
+    div $a1, $t1, $t2                           # $a1 = $t2 / 2
+    jal potencia                                # Chama sub-rotina potencia recursivamente
 
-	lw $ra, 8($sp) 								# Recupera $ra da stack
-	lw $a0, 4($sp) 								# Recupera $a0 da stack
-	lw $a1, 0($sp) 								# Recupera $a1 da stack
-	addi $sp, $sp, 12 							# Encolhe a stack em 12 posições
+    lw $ra, 8($sp)                              # Recupera $ra da stack
+    lw $a0, 4($sp)                              # Recupera $a0 da stack
+    lw $a1, 0($sp)                              # Recupera $a1 da stack
+    addi $sp, $sp, 12                           # Encolhe a stack em 12 posições
 
-	rem $t3, $a1, $t2 							# Verifica se o expoente é par
-	bne $t3, $zero, potencia_expoente_impar 	# Se o expoente for par jump to potencia_expoente_impar
-potencia_expoente_par
-	jr $ra 										# Retorna $v0
+    rem $t3, $a1, $t2                           # Verifica se o expoente é par
+    bne $t3, $zero, potencia_expoente_impar     # Se o expoente for par jump to potencia_expoente_impar
+potencia_expoente_par:
+    jr $ra                                      # Retorna $v0
 potencia_expoente_impar:
-	mul $v0, $v0, $a0 							# $v0 = $v0 * $a0
-	jr $ra 										# Retorna $v0
+    mul $v0, $v0, $a0                           # $v0 = $v0 * $a0
+    jr $ra                                      # Retorna $v0
 potencia_caso_base:
-	move $v0, $a0 								# $v0 = $a0
-	jr $ra 										# Retorna $v0
+    move $v0, $a0                               # $v0 = $a0
+    jr $ra                                      # Retorna $v0
+
 
 raiz_quadrada:
-	move $t0, $a0
-
-	li $v0, 1
-	li $a0, 5
-	syscall
-
-	jr $ra
 
 tabuada:
-	move $t0, $a0
-
-	li $v0, 1
-	li $a0, 6
-	syscall
-
-	jr $ra
 
 imc:
-	move $t0, $a0
-
-	li $v0, 1
-	li $a0, 7
-	syscall
-
-	jr $ra
 
 fatorial:
-	move $t0, $a0
-
-	li $v0, 1
-	li $a0, 8
-	syscall
-
-	jr $ra
 
 fibonacci:
-	move $t0, $a0
 
-	li $v0, 1
-	li $a0, 9
-	syscall
 
-	jr $ra
+# Sub-rotina que recebe como parametros o numero de operandos
+# que devem ser requisitados em $a0, a string que requisita o 
+# primeiro operando em $a1 e se $a0 == 2 a string que requisita
+# o segundo operando. Retorna o primeiro operando em $v0 e caso
+# exista um segundo operando o retorna em $v1
+le_operandos:
+    move $t0, $a0                   # Salva o valor de $a0 em $t0 
+
+    li $v0, 4                       # Valor para syscall imprimir string 
+    move $a0, $a1                   # Carrega a string que requisita o operando1 como argumento para syscall
+    syscall                         
+
+    li $v0, 5                       # Valor para syscall ler um int
+    syscall
+
+    # Se o numero de operandos que deve se ler e apenas um, finaliza operacao
+    bnei $t0, 2 le_operandos_end    # Se $t0 != 2 jump to le_operandos_end
+
+    move $t3, $v0                   # Salva o primeiro operando em $t3
+    
+    li $v0, 4                       # Valor para syscall imprimir string 
+    move $a0, $a1                   # Carrega a string que requisita o operando1 como argumento para syscall
+    syscall                         
+
+    li $v0, 5                       # Valor para syscall ler um int
+    syscall
+
+    move $v1, $v0                   # Passa o valor do segundo operando para $v1
+    move $v0, $t3                   # Retorna o valor do primeiro operando para $v0
+
+le_operandos_end:
+    move $a0, $t0                   # Restaura o valor inicial de $a0
+    jr $ra
+
+
+imprime_resultado:
 
